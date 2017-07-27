812b03134073fb5596af6a22a747a3176aeb9028
da-nie <da-nie@yandex.ru>
2017-07-27 09:26:23 +0400

    Update

--- cframewnd_main.cpp
+++ cframewnd_main.cpp
@@ -1,49 +1,59 @@
 #include "cframewnd_main.h"
 
-//-Функции обработки сообщений класса----------------------------------------
+//====================================================================================================
+//карта сообщений класса
+//====================================================================================================
 BEGIN_MESSAGE_MAP(CFrameWnd_Main,CFrameWnd)
  ON_WM_CREATE()
  ON_WM_TIMER()
  ON_WM_DESTROY()
  ON_WM_SYSCOMMAND()
  ON_MESSAGE(WM_SYSTEM_TRAY_ICON,OnSystemTrayIconMessage)
+ ON_COMMAND(IDC_MENU_SYSTRAY_EXIT,OnCommand_Menu_SysTray_Exit)
 END_MESSAGE_MAP()
-//-Конструктор класса--------------------------------------------------------
+//====================================================================================================
+//конструктор класса
+//====================================================================================================
 CFrameWnd_Main::CFrameWnd_Main(void)
 {
  NotifyIconData.hIcon=NULL;
+ hIcon_SysTray=NULL;
 }
-//-Деструктор класса---------------------------------------------------------
+//====================================================================================================
+//деструктор класса
+//====================================================================================================
 CFrameWnd_Main::~CFrameWnd_Main()
 {
 }
-//-Функции класса------------------------------------------------------------
+//====================================================================================================
+//функции класса
+//====================================================================================================
 
 //----------------------------------------------------------------------------------------------------
 //создание рамки
 //----------------------------------------------------------------------------------------------------
 int CFrameWnd_Main::OnCreate(LPCREATESTRUCT lpCreateStruct)
 {
+ hIcon_SysTray=(HICON)LoadImage(AfxGetInstanceHandle(),(char*)IDI_ICON_MAIN,IMAGE_ICON,32,32,LR_DEFAULTCOLOR);
  //создаём иконку в системном лотке
  NotifyIconData.cbSize=sizeof(NOTIFYICONDATA);
  NotifyIconData.hWnd=m_hWnd;
  NotifyIconData.uID=IDI_ICON_MAIN;
  NotifyIconData.uFlags=NIF_ICON|NIF_MESSAGE|NIF_TIP;
  NotifyIconData.uCallbackMessage=WM_SYSTEM_TRAY_ICON;
- NotifyIconData.hIcon=(HICON)LoadImage(AfxGetInstanceHandle(),(char*)IDI_ICON_MAIN,IMAGE_ICON,32,32,LR_DEFAULTCOLOR);
- strcpy(NotifyIconData.szTip,"Team Control сервер");
+ NotifyIconData.hIcon=hIcon_SysTray; strcpy(NotifyIconData.szTip,"Team Control сервер");
  Shell_NotifyIcon(NIM_ADD,&NotifyIconData);
  SetIcon(NotifyIconData.hIcon,TRUE);  
  return(CFrameWnd::OnCreate(lpCreateStruct));
 }
 //----------------------------------------------------------------------------------------------------
-//уточтожение рамки
+//уничтожение рамки
 //----------------------------------------------------------------------------------------------------
 afx_msg void CFrameWnd_Main::OnDestroy(void)
 {
  Shell_NotifyIcon(NIM_DELETE,&NotifyIconData);
- if (NotifyIconData.hIcon!=NULL) DeleteObject(NotifyIconData.hIcon);
- NotifyIconData.hIcon=NULL;
+ if (hIcon_SysTray!=NULL) DeleteObject(hIcon_SysTray);
+ hIcon_SysTray=NULL;
 }
 //----------------------------------------------------------------------------------------------------
 //событие таймера
@@ -68,7 +78,10 @@ afx_msg void CFrameWnd_Main::OnSysCommand(UINT nID,LPARAM lParam)
  CFrameWnd::OnSysCommand(nID,lParam);
 }
 
-//-Функции обработки сообщений класса----------------------------------------
+//====================================================================================================
+//функции обработки сообщений класса
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 //----------------------------------------------------------------------------------------------------
 //обработка сообщений трея
 //----------------------------------------------------------------------------------------------------
@@ -80,7 +93,28 @@ afx_msg void CFrameWnd_Main::OnSystemTrayIconMessage(WPARAM wParam,LPARAM lParam
   SetForegroundWindow();
   ShowWindow(SW_SHOW);
  }
+ if (lParam==WM_RBUTTONUP)//нажали и отпустили правую кнопку мыши
+ {  
+  SetForegroundWindow();
+  //отображаем меню
+  CMenu *cMenu=new CMenu;
+  cMenu->CreatePopupMenu();
+  cMenu->AppendMenu(MF_STRING,IDC_MENU_SYSTRAY_EXIT,"Завершить работу программы");
+  CPoint cPoint;
+  GetCursorPos(&cPoint);
+  cMenu->TrackPopupMenu(TPM_LEFTBUTTON|TPM_RIGHTBUTTON,cPoint.x,cPoint.y,this);
+  delete(cMenu);
+ }
 }
+//----------------------------------------------------------------------------------------------------
+//обработка команды выхода из программы
+//----------------------------------------------------------------------------------------------------
+afx_msg void CFrameWnd_Main::OnCommand_Menu_SysTray_Exit(void)
+{
+ if (MessageBox("Завершить работу программы?","Подтверждение",MB_YESNO|MB_DEFBUTTON2)!=IDYES) return;
+ DestroyWindow();
+}
+
 //-Прочее--------------------------------------------------------------------
 IMPLEMENT_DYNCREATE(CFrameWnd_Main,CFrameWnd)
 
